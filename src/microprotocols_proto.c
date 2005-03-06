2c656ec9a613685de43cca55fb3b585fd30d14d1
gh <devnull@localhost>
2005-03-06 16:20:01 +0000

    License change.

--- src/microprotocols_proto.c
+++ src/microprotocols_proto.c
@@ -1,22 +1,26 @@
 /* microprotocol_proto.c - psycopg protocols
  *
- * Copyright (C) 2003 Federico Di Gregorio <fog@debian.org>
+ * Copyright (C) 2003-2004 Federico Di Gregorio <fog@debian.org>
  *
- * This file is part of psycopg.
+ * This file is part of psycopg and was adapted for pysqlite. Federico Di
+ * Gregorio gave the permission to use it within pysqlite under the following
+ * license:
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2,
- * or (at your option) any later version.
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 #include <Python.h>
@@ -195,7 +199,11 @@ PyTypeObject isqlquoteType = {
     0,          /*tp_dictoffset*/
     
     isqlquote_init, /*tp_init*/
+#if 0
     PyType_GenericAlloc, /*tp_alloc*/
+#else
+    0,                   /* tp_alloc */
+#endif
     isqlquote_new, /*tp_new*/
     (freefunc)isqlquote_del, /*tp_free  Low-level free-memory routine */
     0,          /*tp_is_gc For PyObject_IS_GC */
@@ -205,3 +213,8 @@ PyTypeObject isqlquoteType = {
     0,          /*tp_subclasses*/
     0           /*tp_weaklist*/
 };
+
+extern void microprotocols_proto_init()
+{
+    isqlquoteType.tp_alloc = PyType_GenericAlloc;
+}
