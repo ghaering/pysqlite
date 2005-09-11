65163758203a27100620a824841bf627360cd561
gh <devnull@localhost>
2005-09-11 20:51:06 +0000

    Script for Windows to set the environment variables suitable for compiling pysqlite.

---
+++ win32env.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem Set environment variables for the MS Toolkit compiler on Windows
+Set PATH=C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin;C:\Program Files\Microsoft Platform SDK for Windows XP SP2\Bin;%PATH%
+Set INCLUDE=C:\Program Files\Microsoft Visual C++ Toolkit 2003\include;C:\Program Files\Microsoft Platform SDK for Windows XP SP2\Include;c:\python24\include;%INCLUDE%
+Set LIB=C:\Program Files\Microsoft Platform SDK for Windows XP SP2\Lib;C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\lib;C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib;C:\Program Files\Microsoft.NET\SDK\v1.1\Lib;c:\python24\libs;%LIB%;
+
