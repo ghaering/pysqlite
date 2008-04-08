5cc8f85bb5bd3c38ac0d17d8e6e1b8f7429e3643
gh <gh@ghaering.de>
2008-04-08 11:07:59 +0200

    Download SQLite amalgamation if it isn't there, already.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
+import urllib
+import zipfile
 
 long_description = \
 """Python interface to SQLite 3
@@ -12,8 +15,9 @@ def get_setup_args():
     py_modules = ["sqlite"]
 
     ext = Extension(
-        "_pysqlite4",
-        ["sqlite.pyx", "amalgamation/sqlite3.c"]
+        name = "_pysqlite4",
+        sources = ["sqlite.pyx", "amalgamation/sqlite3.c"],
+        include_dirs = ["amalgamation"]
     )
 
     setup_args = dict(
@@ -50,7 +54,27 @@ def get_setup_args():
             )
     return setup_args
 
+def get_amalgamation():
+    """Download the SQLite amalgamation if it isn't there, already."""
+    AMALGAMATION_ROOT = "amalgamation"
+    if os.path.exists(AMALGAMATION_ROOT):
+        return
+    os.mkdir(AMALGAMATION_ROOT)
+    print "Downloading amalgation."
+    urllib.urlretrieve("http://sqlite.org/sqlite-amalgamation-3_5_7.zip", "tmp.zip")
+    zf = zipfile.ZipFile("tmp.zip")
+    files = ["sqlite3.c", "sqlite3.h"]
+    for fn in files:
+        print "Extracting", fn
+        outf = open(AMALGAMATION_ROOT + os.sep + fn, "wb")
+        outf.write(zf.read(fn))
+        outf.close()
+    zf.close()
+    os.unlink("tmp.zip")
+
+
 def main():
+    get_amalgamation()
     setup(**get_setup_args())
 
 if __name__ == "__main__":
