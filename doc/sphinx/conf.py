3042185ebba722d9142c59d812ff95d33efd2d88
Gerhard Haering <gh@ghaering.de>
2008-09-05 23:05:47 +0200

    - updated version in documentation - fixed script that uploads docs to the pysqlite homepage

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -40,9 +40,9 @@ copyright = '2008, Gerhard Häring'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.4.1'
+version = '2.5.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.4.1'
+release = '2.5.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
