4d1fb4b97185e159bc8b7f4dc79addedd974db8f
Gerhard Haering <devnull@localhost>
2008-10-28 17:00:57 +0100

    - build Windows binaries for Python 2.6, too. - build Windows binaries with fulltext search enabled.

--- mkwin32.py
+++ mkwin32.py
@@ -42,13 +42,13 @@ def compile_module(pyver):
     INC = "%s/python%s/include" % (CROSS_TOOLS, VER)
     vars = locals()
     vars.update(globals())
-    cmd = '%(CC)s -mno-cygwin -mdll -DMODULE_NAME=\\"pysqlite2._sqlite\\" -I amalgamation -I %(INC)s -I . %(SRC)s -L %(CROSS_TOOLS)s/python%(VER)s/libs -lpython%(VER)s -o build/lib.linux-i686-2.5/pysqlite2/_sqlite.pyd' % vars
+    cmd = '%(CC)s -mno-cygwin -mdll -DMODULE_NAME=\\"pysqlite2._sqlite\\" -DSQLITE_ENABLE_FTS3=1 -I amalgamation -I %(INC)s -I . %(SRC)s -L %(CROSS_TOOLS)s/python%(VER)s/libs -lpython%(VER)s -o build/lib.linux-i686-2.5/pysqlite2/_sqlite.pyd' % vars
     os.system(cmd)
     os.system("strip --strip-all build/lib.linux-i686-2.5/pysqlite2/_sqlite.pyd")
 
 def main():
     get_amalgamation()
-    for ver in ["2.3", "2.4", "2.5"]:
+    for ver in ["2.3", "2.4", "2.5", "2.6"]:
         os.system("rm -rf build")
         # First, compile the Linux version. This is just to get the .py files in place.
         os.system("python2.5 setup.py build")
