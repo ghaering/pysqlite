694a1ab68f582d2d1b9697b29e92e06517123c37
Gerhard Haering <gh@ghaering.de>
2009-11-03 10:24:12 +0100

    Bump version number.

--- src/module.h
+++ src/module.h
@@ -25,7 +25,7 @@
 #define PYSQLITE_MODULE_H
 #include "Python.h"
 
-#define PYSQLITE_VERSION "2.5.5"
+#define PYSQLITE_VERSION "2.5.6"
 
 extern PyObject* pysqlite_Error;
 extern PyObject* pysqlite_Warning;
