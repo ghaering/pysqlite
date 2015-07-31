18960b8449fdbffecc972f5879b05a76de83f64d
Gerhard Haering <gh@ghaering.de>
2015-07-31 22:58:23 +0200

    Make sure we can still build on systems where there is no pkg-config.

--- setup.py
+++ setup.py
@@ -113,8 +113,11 @@ class MyBuildExt(build_ext):
             ext.define_macros.append(("SQLITE_ENABLE_FTS3", "1"))
             ext.define_macros.append(("SQLITE_ENABLE_RTREE", "1"))
             ext.sources.append("sqlite3.c")
-        ext.include_dirs = self._pkgconfig_include_dirs("sqlite3")
-        ext.library_dirs = self._pkgconfig_library_dirs("sqlite3")
+        try:
+            ext.include_dirs = self._pkgconfig_include_dirs("sqlite3")
+            ext.library_dirs = self._pkgconfig_library_dirs("sqlite3")
+        except OSError:
+            pass # no pkg_config installed
         build_ext.build_extension(self, ext)
 
     def __setattr__(self, k, v):
