b1375ce7ac83b7936c7cae453f040bcd95ebc1b9
palaviv <palaviv@gmail.com>
2016-01-29 17:37:42 +0200

    example scripts added

---
+++ doc/includes/sqlite3/blob_with.py
@@ -0,0 +1,12 @@
+from pysqlite2 import dbapi2 as sqlite3
+con = sqlite3.connect(":memory:")
+# creating the table
+con.execute("create table test(id integer primary key, blob_col blob)")
+con.execute("insert into test(blob_col) values (zeroblob(10))")
+# opening blob handle
+with con.blob("test", "blob_col", 1, 1) as blob:
+    blob.write("a" * 5)
+    blob.write("b" * 5)
+    blob.seek(0)
+    print blob.read() # will print "aaaaabbbbb" 
+
